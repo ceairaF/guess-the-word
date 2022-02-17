83ca7b4d2e0ea1e5a1c60cf25d48d7e1fa13d130
ceaira.f <ceaira.f@gmail.com>
2022-02-17 10:50:30 -0500

    Accept and Validate Player Guesses

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters= document.querySelector(".guessed-letters");
+const guessedLettersElement= document.querySelector(".guessed-letters");
 const guessButton= document.querySelector(".guess");
 const letterInput= document.querySelector(".letter");
 const wordInProgress= document.querySelector(".word-in-progress");
@@ -10,7 +10,7 @@ const playAgain= document.querySelectorAll(".play-again hide");
 
 //function to add placeholders for each letter
 const word= "magnolia";
-
+const guessedLetters= [];
 
 const placeholder= function(word){
     const placeholderLetters= [];
@@ -25,13 +25,48 @@ wordInProgress.innerText= placeholderLetters.join("");
 placeholder(word);
 
 //Add an Event Listener for the button
-
 guessButton.addEventListener("click", function(e){
     e.preventDefault();
-    const value= letterInput.value;
-    console.log(value);
-    letterInput.value = "";
+    message.innerText = "";
+    const guess= letterInput.value;
+    //console.log(value);
+    const goodGuess= validateInput(guess);
+
+    if (goodGuess) {
+        makeGuess(guess);
+    }
+    letterInput.value="";
 });
+
+//Function to check players input
+
+const validateInput= function(input) {
+    const acceptedLetter = /[a-zA-Z]/
+    if (input.length === 0){
+        message.innerText="Please enter a letter from A-Z.";
+
+    }else if (input.length >1){
+        message.innerText="Only use one letter,please!";
+
+    }else if (!input.match(acceptedLetter)){
+        message.innerText="Please enter a letter from A-Z.";
+
+    }else {
+        return input;
+    }
+
+}
+
+const makeGuess=function(guess){
+    guess=guess.toUpperCase();
+if(guessedLetters.includes(guess)) {
+    message.innerText = "Already tried that one! Try again."
+}else {
+    guessedLetters.push(guess);
+    console.log(guessedLetters);
+}
+
+};
 
 
 
